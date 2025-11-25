3d72511b92b16db6e50c55c3600f52a3a0824600
Poll_00 <pollyasiel@gmail.com>
2025-11-25 10:03:26 -0500

    Changing to postgresql db

--- vetclinic/settings.py
+++ vetclinic/settings.py
@@ -157,8 +157,12 @@ WSGI_APPLICATION = 'vetclinic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', default="VetClinicDB"),
+        "USER": os.getenv('DB_USER', default="postgres"),
+        "PASSWORD": os.getenv('DB_PASSWORD', default="postgres"),
+        "HOST": os.getenv('DB_HOST', default="127.0.0.1"),
+        "PORT": os.getenv('DB_PORT', default="5432"),
     }
 }
 
